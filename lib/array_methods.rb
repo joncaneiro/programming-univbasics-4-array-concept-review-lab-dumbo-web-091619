390bdb641a1379db79741c61801acb475a52aa88
Jon Caneiro <joncaneiro@gmail.com>
2019-09-08 21:04:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,11 +7,11 @@ def find_element_index(array, value_to_find)
     
     if array[i] == value_to_find
       
-      return i
+      puts i
       
     elsif array[i] != value_to_find
       
-      return nil 
+      puts nil 
       
     end
     
